b46f1b23088f02b5820021ca48bf958bb016b06e
Cybear Tron <vinutron23@gmail.com>
2022-01-14 10:17:36 +0530

    Added all the questions. Will add all the paths soon.

--- godot/2.gd
+++ godot/2.gd
@@ -3,14 +3,14 @@ extends Node2D
 var current = 0
 
 var questions = {
-	"questions":["If your mom was dying would you kill a thousand people to find the cure?","If you kill your family you live. But if you don't you die. Would you kill them?","How do you think of yourself? Bad or Good?","If you could have a million dollars every day or become a devotee of god and go to heaven, what would you choose?"]
+	"questions":["If you saw god then will you doubt yourself?","If your mom was dying would you kill a thousand people to find the cure?","If you kill your family you live. But if you don't you die. Would you kill them?","How do you think of yourself? Bad or Good?","If you could have a million dollars every day or become a devotee of god and go to heaven, what would you choose?"]
 }
 var answers_1 = {
-	"answers":["Yes","Yep","Good","God's Devotee","YES"]
+	"answers":["YES","Yes","Yep","Good","God's Devotee"]
 }
 
 var answers_2 = {
-	"answers":["No","Nope","Bad","Million Dollars","NO"]
+	"answers":["NO","No","Nope","Bad","Million Dollars"]
 }
 
 
@@ -26,13 +26,13 @@ func _physics_process(delta: float) -> void:
 
 func _on_Yes_pressed() -> void:
 	$Storer.data.append($Yes.text)
-	if current <=2:
+	if current <=3:
 		
 		current +=1
 
 
 func _on_No_pressed() -> void:
 	$Storer.data.append($No.text)
-	if current <=2:
+	if current <=3:
 		
 		current +=1
